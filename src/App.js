b911ba7fca428548927117027ed2ef8fa55c6ffa
Megan D <45672063+megan-d@users.noreply.github.com>
2019-02-04 10:29:41 -0700

    add scroll component and functionality for sticky searchbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 //create App componenent that will be the overall app and will contain the SearchBox and CardList components
 
@@ -39,7 +40,10 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filteredRobots}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                    
                 </div>
                 
             );
