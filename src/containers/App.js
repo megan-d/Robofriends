d6ed00c1c89b2c23e22e93e2a09e8088ab7e86b2
Megan D <45672063+megan-d@users.noreply.github.com>
2019-02-05 11:07:28 -0700

    create folder structure and clean up code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 //create App componenent that will be the overall app and will contain the SearchBox and CardList components
-
 
 //called a smart component because it has state
 class App extends Component {
@@ -30,13 +29,12 @@ class App extends Component {
         
     }
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+            return robots.length === 0 ? <h1>Loading</h1> : 
+            (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
@@ -47,7 +45,6 @@ class App extends Component {
                 </div>
                 
             );
-        }
     }
     
 }
